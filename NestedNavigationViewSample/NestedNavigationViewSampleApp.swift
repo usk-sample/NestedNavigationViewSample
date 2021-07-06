9b4b4ce64b11988a49ec86dad0ec305609fac29e
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-07-06 09:06:33 +0900

    Initial Commit

---
+++ NestedNavigationViewSample/NestedNavigationViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestedNavigationViewSampleApp.swift
+//  NestedNavigationViewSample
+//
+//  Created by Yusuke Hasegawa on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct NestedNavigationViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
